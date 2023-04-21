11611fc4ba333410f88c1c85d6f19c8713c59d24
Kunal Mangal <kmangal0991@gmail.com>
2023-04-21 09:49:15 +0530

    Update test.bat
    Add reference

--- test.bat
+++ test.bat
@@ -1,1 +1,2 @@
+:: Reference: https://docs.github.com/en/pages/setting-up-a-github-pages-site-with-jekyll/testing-your-github-pages-site-locally-with-jekyll
 bundle exec jekyll serve --config "_config.yml,_config_dev.yml"
